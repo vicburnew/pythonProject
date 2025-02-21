5046d268994ca0cc8b684d6b66bc2f05744d0018
Victor Burtsev <vb4oot@gmail.com>
2025-02-21 10:25:35 +0300

    в файле main.py добавлена функция divide

--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
-import os
-import os.path
-from typing import Union
-
-from untils.hello import hello
-
-
-def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    """Функция, которая складывает два числа"""
-    return a + b
-
-
-
-from untils.add import add
-
-print(add(6, 2))
-help(add)
-help(hello)
+# import os
+# import os.path
+# from typing import Union
+#
+# from untils.hello import hello
+#
+#
+# def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+#     """Функция, которая складывает два числа"""
+#     return a + b
+#
+#
+#
+# from untils.add import add
+#
+# print(add(6, 2))
+# help(add)
+# help(hello)
 
 ## добавлена ветка develop
+
+def divide (a, b):
+    if b > 0:
+        return  a / b
+    return 0
 
 
 
