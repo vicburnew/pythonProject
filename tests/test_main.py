50cac82184477672cd5e7e665bd65fb8ff2dfea9
Victor Burtsev <vb4oot@gmail.com>
2025-02-21 14:56:07 +0300

    в файл test_main.py добавлены функции тестирования reverse_list для numbers и letters

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import divide
+from src.main import divide, reverse_list
 
 
 def test_divide():
@@ -7,5 +7,8 @@ def test_divide():
 def test_zero_divide():
     assert divide(2, 0) == 0
 
+def test_reverse_list_numbers():
+    assert reverse_list("123") == "321"
 
-
+def test_reverse_list_letters():
+    assert reverse_list("hello") == "olleh"
